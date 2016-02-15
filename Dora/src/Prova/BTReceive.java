cfc0b6386338c16dbf53dc4ea652cb0ab24132c8
ivy_s <ivy_s@192.168.1.162>
2016-02-15 11:01:32 +0100

    bluetooth hecho

--- Dora/src/Prova/BTReceive.java
+++ Dora/src/Prova/BTReceive.java
@@ -4,35 +4,73 @@ import lejos.nxt.*;
 import lejos.nxt.comm.*;
 
 public class BTReceive {
-  public static void main(String [] args) throws Exception {
-    String connected = "Connected";
-    String waiting = "Waiting...";
-    String closing = "Closing...";
-    TouchSensor tS = new TouchSensor(SensorPort.S1);
-    
-    while (!tS.isPressed()) {
-      LCD.drawString(waiting,0,0);
-      NXTConnection connection = Bluetooth.waitForConnection(); 
-      LCD.clear();
-      LCD.drawString(connected,0,0);
+	 public static void main(String [] args) throws Exception
+	 {
+	 String connected = "Connected";
+	 String waiting = "Waiting...";
+	 String closing = "Closing...";
+	 
+//	 M[0,0]=true;
+	 
+	 while (true)
+	 {
+	 LCD.drawString(waiting,0,0);
+	 BTConnection btc = Bluetooth.waitForConnection();
 
-      DataInputStream dis = connection.openDataInputStream();
-      DataOutputStream dos = connection.openDataOutputStream();
+	 LCD.clear();
+	 LCD.drawString(connected,0,0);
+	 DataInputStream dis = btc.openDataInputStream();
+	 DataOutputStream dos = btc.openDataOutputStream();
 
-      for(int i=0;i<100;i++) {
-        int n = dis.readInt();
-        LCD.drawInt(n,7,0,1);
-        dos.writeInt(-n);
-        dos.flush();
-      }
-      dis.close();
-      dos.close();
+//	 for(int i=0;i<100;i++) {
+		 String n = dis.readUTF();
+		 LCD.drawString(n, 7, 1);
+//		 dos.writeInt(-n);
+		 dos.writeInt(42);
+		 dos.flush();
+//	 }
 
-      LCD.clear();
-      LCD.drawString(closing,0,0);
+	 dis.close();
+	 dos.close();
+	 Thread.sleep(100); // wait for data to drain
+	 LCD.clear();
+	 LCD.drawString(closing,0,0);
+	 btc.close();
+	 LCD.clear();
+	 }
+	 }
+	} 
 
-      connection.close();
-      LCD.clear();
-    }
-  }
-}
+//public class BTReceive {
+//  public static void main(String [] args) throws Exception {
+//    String connected = "Connected";
+//    String waiting = "Waiting...";
+//    String closing = "Closing...";
+//    TouchSensor tS = new TouchSensor(SensorPort.S1);
+//    
+//    while (!tS.isPressed()) {
+//      LCD.drawString(waiting,0,0);
+//      NXTConnection connection = Bluetooth.waitForConnection(); 
+//      LCD.clear();
+//      LCD.drawString(connected,0,0);
+//
+//      DataInputStream dis = connection.openDataInputStream();
+//      DataOutputStream dos = connection.openDataOutputStream();
+//
+//      for(int i=0;i<100;i++) {
+//        int n = dis.readInt();
+//        LCD.drawInt(n,7,0,1);
+//        dos.writeInt(-n);
+//        dos.flush();
+//      }
+//      dis.close();
+//      dos.close();
+//
+//      LCD.clear();
+//      LCD.drawString(closing,0,0);
+//
+//      connection.close();
+//      LCD.clear();
+//    }
+//  }
+//}
