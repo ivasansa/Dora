82e2b5eb1a77e86c873f85a2d0412ca01d48f259
ivy_s <ivy_s@192.168.1.130>
2016-03-02 11:46:05 +0100

    Javadoc fet

--- Dora/src/Explorando/Main.java
+++ Dora/src/Explorando/Main.java
@@ -4,6 +4,9 @@ import lejos.nxt.Button;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * Clase Main, que efectúa la ejecución del Robot
+ */
 public class Main {
 	
 	public static void main(String[] args) {
@@ -11,14 +14,31 @@ public class Main {
 		LinkedList<Posicio> mapa = new LinkedList<>();
 			BT bt = new BT();
 			int entrat = 0;
+			
+			/**
+			 * Función que mantiene el Robot en standby hasta que se recibe desde el PC una señal 
+			 * de inicio "init"
+			 */
 			while(bt.RecibeInit()){}
 
+			/**
+			 * Funcionamiento lógico del Robot que se ejecuta cuando se recibe un init.
+			 */
 			while(bt.RecibePush(mapa)){
 				mapa.add(new Posicio(1,1,0)); //Posem la posició actual del robot com a 
 				Queue<Integer> q = new Queue<Integer>();
+				
+				/**
+				 * Sólo queremos entrar en el modo de giro 1 vez
+				 */
 				if(entrat == 0){
+					/**
+					 * Bucle que gira el robot 8 veces
+					 */
 					for(int i = 1; i <= 8; i++){
-						
+						/**
+						 * Bloque que mapea la posición de enfrente del Robot
+						 */
 						try {
 							q = Robot.parsePosicion(i);
 						} catch (ParseException e) {
@@ -29,9 +49,20 @@ public class Main {
 						y = (int) q.pop();
 						mapa.add(new Posicio(x, y, Robot.FrontOcupat()));
 						
+						/**
+						 * Giramos el robot
+						 */
 						Robot.Girar(45);
 						Robot.Parar();
+						
+						/**
+						 * Flag para evitar que el robot gire más de 8 veces
+						 */
 						entrat = 1;
+						
+						/**
+						 * Reseteamos la cola del parsing
+						 */
 						q.clear();
 					}
 				}
--- Dora/src/Explorando/ParseException.java
+++ Dora/src/Explorando/ParseException.java
@@ -1,5 +1,9 @@
 package Explorando;
 
+/**
+ * Excepció personalitzada que tracta un error en el Parse del encarament del Robot relativa a 
+ * la coordenada xy del mapa a realitzar
+ */
 public class ParseException extends Exception {
 	public ParseException(String message){
 		super(message);
